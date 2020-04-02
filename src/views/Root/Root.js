bf6d298531a9f63fd9bc05760b40343027797866
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-04-02 18:20:47 +0200

    Basics

---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <>
+    <h1>Hello!</h1>
+    <Button>Close/save</Button>
+    <Button secondary>Remove</Button>
+  </>
+);
+
+export default Root;
