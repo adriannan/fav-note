4f6bc2d0c32acc748d34df26f98653f6f932403d
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-04-03 13:07:11 +0200

    fixes

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Button from 'components/Button/Button';
+import { ThemeProvider } from 'styled-components';
+import Input from 'components/atoms/Input';
+import Card from 'components/molecules/Card';
+import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
 
 const Root = () => (
   <>
-    <h1>Hello!</h1>
-    <Button>Close/save</Button>
-    <Button secondary>Remove</Button>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello!</h1>
+        <Input placeholder="input" />
+        <Input search placeholder="search" />
+        <Card cardType="twitter" />
+        <Card cardType="article" />
+      </>
+    </ThemeProvider>
   </>
 );
 
