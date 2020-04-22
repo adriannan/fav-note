aa1f9873887a47581b5e821db6218705f9075d00
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-04-22 23:25:24 +0200

    compose redux-thunk & dev-tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from 'reducers';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducer,
-  /* preloadedState, */ window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 /* eslint-enable */
 
 export default store;
