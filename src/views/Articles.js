623c1898edf37a4658cee890566ea24bf8de3df7
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2020-04-23 16:26:56 +0200

    validate props

--- src/views/Articles.js
+++ src/views/Articles.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card';
@@ -19,7 +20,20 @@ const Articles = ({ articles }) => {
     </GridTemplate>
   );
 };
+Articles.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+      created: PropTypes.string.isRequired,
+    }),
+  ),
+};
 
+Articles.defaultProps = {
+  articles: [],
+};
 const mapStateToProps = ({ articles }) => ({ articles });
 
 export default connect(mapStateToProps)(Articles);
